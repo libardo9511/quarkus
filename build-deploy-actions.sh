c8fbdd6eaa3ab6589e85110f573c2459236a27ec
Lmora <libardo9511@hotmail.com>
2024-09-15 13:10:25 -0500

    Ajuste script railway

--- build-deploy-actions.sh
+++ build-deploy-actions.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
-cd deploy-actions
-./mvnw package  # Para Maven
+# Configura el nombre de la imagen
+IMAGE_NAME="my-docker-image"
+TAG="latest"  # Puedes usar una etiqueta específica si lo deseas
+
+# Construye la imagen Docker
+docker build -t $IMAGE_NAME:$TAG .
