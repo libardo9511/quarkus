427b0a9d136cdbdbf4ae6db3eccf40eddf8434da
Lmora <libardo9511@hotmail.com>
2024-09-15 13:30:52 -0500

    Delete script railway

--- build-deploy-actions.sh
+++
@@ -1,6 +0,0 @@
-# Configura el nombre de la imagen
-IMAGE_NAME="my-docker-image"
-TAG="latest"  # Puedes usar una etiqueta específica si lo deseas
-
-# Construye la imagen Docker
-docker build -t $IMAGE_NAME:$TAG .
